bf3dec4672580845f21ef98cbc63e62bfe54d4aa
Sadig Latifli <76481289+SadigLatifli@users.noreply.github.com>
2026-08-07 02:16:32 +0400

    Enable GSAP story snapping

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { MapStory } from "../components/map-story/MapStory";
+import { StorySnapController } from "../components/map-story/StorySnapController";
 
 export default function Home() {
-  return <MapStory />;
+  return (
+    <>
+      <StorySnapController />
+      <MapStory />
+    </>
+  );
 }
